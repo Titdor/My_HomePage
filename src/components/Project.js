6d70936d45eeeef551238518d5af3e8ca6d9fa9c
unknown <theodorpanguripan@gmail.com>
2023-02-26 21:59:55 +0800

    fixing text

--- src/components/Project.js
+++ src/components/Project.js
@@ -43,7 +43,7 @@ const Projects = () => {
                 {({ isVisible }) =>
                     <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                         <h2>Projects</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p>Below are previews of my projects I have made so far.</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
                             <Tab.Content id="slideInUp">
                             <Tab.Pane eventKey="first">
